2460c9570f880ed3ea8da19f4ac1282e7dd8cd9d
bipinpromoty <bipin@promoty.eu>
2021-06-27 15:10:18 +0545

    first

---
+++ adventures/custom_slide_drawer/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(CustomerSliderApp());
+}
+
+class CustomerSliderApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+              ),
+              child: Center(
+                child: Text("hey"),
+              )
+            ),
+            // Container(
+            //   decoration: BoxDecoration(
+            //     color: Colors.greenAccent,
+            //   ),
+            //   child: Center(
+            //     child: Text("cool app!"),
+            //   ),
+            // )
+          ],
+        )
+      ),
+    );
+  }
+}
---
+++ adventures/test_proj_just_delete_me/android/app/src/main/kotlin/com/example/test_proj_just_delete_me/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_proj_just_delete_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
